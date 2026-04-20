fca0a1f726e1be14fa31a85feb3f23b98dbd7005
helloheu <96169764+helloheu@users.noreply.github.com>
2026-04-20 10:50:21 +0800

    feat: export source scrapers

---
+++ scraper/sources/__init__.py
@@ -0,0 +1,9 @@
+from scraper.sources.kiwi import KiwiScraper
+from scraper.sources.rail_cn import ChinaRailScraper
+from scraper.sources.skyscanner import SkyscannerScraper
+
+__all__ = [
+    "ChinaRailScraper",
+    "KiwiScraper",
+    "SkyscannerScraper",
+]
